8f03dbfad1ae3d68a51ba5f4a210933bcffaa135
Pranab Samanta <pranabsamanta381@gmail.com>
2026-06-18 20:25:00 +0530

    just cahnge align

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,37 +1,39 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware =
-async (req, res, next) => {
+  async (req, res, next) => {
 
-  try {
+    console.log(req.headers.authorization);
 
-    const token =
-      req.headers.authorization?.split(" ")[1];
+    try {
 
-    if (!token) {
+      const token =
+        req.headers.authorization?.split(" ")[1];
+
+      if (!token) {
+        return res.status(401).json({
+          message: "No token provided"
+        });
+      }
+
+      const decoded =
+        jwt.verify(
+          token,
+          process.env.JWT_SECRET
+        );
+
+      req.user = decoded;
+
+      next();
+
+    } catch (error) {
+
       return res.status(401).json({
-        message: "No token provided"
+        message: "Invalid token"
       });
+
     }
 
-    const decoded =
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET
-      );
-
-    req.user = decoded;
-
-    next();
-
-  } catch (error) {
-
-    return res.status(401).json({
-      message: "Invalid token"
-    });
-
-  }
-
-};
+  };
 
 export default authMiddleware;
