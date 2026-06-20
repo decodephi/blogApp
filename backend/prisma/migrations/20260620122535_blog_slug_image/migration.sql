e387534c8a0782b0cf5b2bd0f68e24b3cf3c88b8
Pranab Samanta <pranabsamanta381@gmail.com>
2026-06-20 17:56:32 +0530

    migration update

---
+++ backend/prisma/migrations/20260620122535_blog_slug_image/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[slug]` on the table `Blog` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `slug` to the `Blog` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `blog` ADD COLUMN `coverImage` VARCHAR(191) NULL,
+    ADD COLUMN `slug` VARCHAR(191) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Blog_slug_key` ON `Blog`(`slug`);
