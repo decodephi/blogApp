159608b92977fc1dadaa4ffcde0845032850811e
Pranab Samanta <pranabsamanta381@gmail.com>
2026-06-21 23:08:16 +0530

    add like controoler

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -1,19 +1,128 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "../config/prisma.js";
 
-const prisma = new PrismaClient();
+export const likeBlog = async (req, res) => {
 
+    try {
 
-export const likeBlog = async (req,res)=>{
+        const existingLike =
+            await prisma.like.findFirst({
+                where: {
+                    userId: req.user.id,
+                    blogId: req.params.id
+                }
+            });
 
- const like = await prisma.like.create({
+        if (existingLike) {
+            return res.status(400).json({
+                message: "Already liked"
+            });
+        }
 
-   data:{
-     userId:req.user.id,
-     blogId:req.params.id
-   }
+        const like =
+            await prisma.like.create({
+                data: {
+                    userId: req.user.id,
+                    blogId: req.params.id
+                }
+            });
 
- });
+        await prisma.blog.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                likes: {
+                    increment: 1
+                }
+            }
+        });
 
- res.json(like);
+        res.status(201).json({
+            message: "Blog liked",
+            like
+        });
 
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
+export const unlikeBlog = async (req, res) => {
+
+    try {
+
+        const like =
+            await prisma.like.findFirst({
+                where: {
+                    userId: req.user.id,
+                    blogId: req.params.id
+                }
+            });
+
+        if (!like) {
+            return res.status(404).json({
+                message: "Like not found"
+            });
+        }
+
+        await prisma.like.delete({
+            where: {
+                id: like.id
+            }
+        });
+
+        await prisma.blog.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                likes: {
+                    decrement: 1
+                }
+            }
+        });
+
+        res.status(200).json({
+            message: "Like removed"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
+export const getLikes = async (req, res) => {
+
+    try {
+
+        const likes =
+            await prisma.like.findMany({
+
+                where: {
+                    userId: req.user.id
+                },
+
+                include: {
+                    blog: true
+                }
+
+            });
+
+        res.json(likes);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
 };
