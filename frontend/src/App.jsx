a7ab0ceffb7cd52c8484e386fb00ea63370d7254
Pranab Samanta <pranabsamanta381@gmail.com>
2026-06-23 12:22:32 +0530

    emni cahnge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,41 @@
-import { useEffect, useState } from "react";
-import API from "./api/axios";
+import Navbar
+  from "./components/Navbar";
+
+import Home
+  from "./pages/Home";
+
+import BlogDetails
+  from "./pages/BlogDetails";
+
+import {
+  Routes,
+  Route
+}
+  from "react-router-dom";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const fetchBackend = async () => {
-      try {
-        const response = await API.get("/");
-
-        setMessage(response.data.message);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchBackend();
-  }, []);
 
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold">
-        {message}
-      </h1>
-    </div>
+
+    <>
+      <Navbar />
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/blog/:slug"
+          element={<BlogDetails />}
+        />
+
+      </Routes>
+
+    </>
+
   );
 }
 
